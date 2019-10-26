d1bd28deb0da256e089530ed9ce237fc96fa4c75
waspryo <wasp2123321@gmail.com>
2019-10-26 22:32:44 +0900

    add image and info about instagram page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/logo.png" style="height: 120px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Joker</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>238</strong> posts</div>
+                <div class="pr-5"><strong>15k</strong> followers</div>
+                <div class="pr-5"><strong>0</strong> following</div>
             </div>
         </div>
     </div>
