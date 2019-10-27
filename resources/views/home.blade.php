f254ff518e749aa0a41057d92cdf0a1f29c5cba2
waspryo <wasp2123321@gmail.com>
2019-10-27 22:57:13 +0900

    parsing data t the view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img src="/svg/logo.png" style="height: 120px;">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>Joker</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>238</strong> posts</div>
                 <div class="pr-5"><strong>15k</strong> followers</div>
