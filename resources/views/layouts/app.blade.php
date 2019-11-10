23223c2933ceccee1a99d08734813830f2305241
waspryo <wasp2123321@gmail.com>
2019-11-10 17:42:04 +0900

    add Procfile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
                 <div><img src="/svg/logo.png" style="height: 37px; border-right: 1px solid #333;" class="pr-3"></div>   
-                <div class="pl-3 pt-1">freeCodeGram</div>
+                <div class="pl-3 pt-1">Instagram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
